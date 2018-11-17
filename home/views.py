d3d8fafea9163ec24861c10dc9711e191fe08167
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2018-11-17 21:37:51 +0700

    sds

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from places.models import Place
+from house.models import House
 # Create your views here.
 def home(request):
-    return render(request,'home/home.html')
+    places = Place.objects.all().order_by('-id')[:8]
+    houses = House.objects.all().order_by('-id')[:3]
+    if 'account' in request.session:
+        idempresa = request.session['account']
+    else:
+        idempresa=None
+    context = {
+        'places':places,
+        'idempresa':idempresa,
+        'houses':houses
+    }
+    return render(request,'home/home.html',context)
